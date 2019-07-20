9142d40d4e247c43959a5932acad9bfd3a2991b5
Hurma H <hurmahm@gmail.com>
2019-07-20 07:28:39 -0500

    screenshot added

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,20 +1,33 @@
 package com.cybertek.step_definitions;
 
 import com.cybertek.utilities.Driver;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
     @Before(order =2)
     public void setUp() {
         System.out.println("I am setting up the test from the Hooks class!!!");
-        Driver
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
         System.out.println("Ia m reporting results!!!");
+
+        if(scenario.isFailed()) {
+            final byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+
+        System.out.println("Closing driver");
+        Driver.getDriver().close();
     }
 
     @Before (value="@wip", order =11)
